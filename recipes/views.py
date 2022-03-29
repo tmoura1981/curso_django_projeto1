4ff6a1058544babda3e57b665898adb7eae66c93
Thiago Moura <tmoura1981@gmail.com>
2022-03-29 00:34:09 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
-# Create your views here.
 def home(request):
-    # home.html é tudo o que será redenrizado
-    # estando dentro da pasta templates, uma subpasta de Recipes
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Thiago'
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
     })
