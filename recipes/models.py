aa38abb818eed2a6bea9899b431219cdd477d9f1
Thiago Moura <tmoura1981@gmail.com>
2022-06-30 14:35:30 -0300

    Criação do Crud

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from distutils.command.upload import upload
+from turtle import title, update
+from unicodedata import category, name
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from typing_extensions import Self
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=180)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=60)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=60)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True)
