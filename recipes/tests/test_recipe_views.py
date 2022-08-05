d9be67c15edf7aeb42faafce764165532f524d8e
Thiago Moura <tmoura1981@gmail.com>
2022-08-04 21:17:51 -0300

    Teste de Context e Content da Response em Template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -64,4 +64,9 @@ class RecipeViewsTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
-        assert 1 == 1
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        self.assertEqual(len(response_context_recipes), 1)
+        self.assertIn('Recipe Title', content)
+        pass
